963bf4bf991b897bd685cb306428831f54c522fc
cuervo-blanco <jaios.228@gmail.com>
2024-06-09 10:47:13 -0400

    modules placeholder members

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use wt_tools::audio;
+use wt_tools::communication;
+use wt_tools::discovery;
+use wt_tools::db;
+use wt_tools::metadata;
+
 fn main() {
-    println!("Hello, world!");
+    // Initializ necessary components
+    audio::initialize_audio_interface();
+    communication::initialize_webrtc();
+    discovery::start_discovery();
+    db::initialize_database();
 }
